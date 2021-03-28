8bd183bb11002693940828b34d7bc5657faf6942
Amanda-Chang <78400863+Amanda-Chang@users.noreply.github.com>
2021-03-28 15:35:23 +0800

    Update language.cpp

--- language.cpp
+++ language.cpp
@@ -1,21 +1,21 @@
-// #include<iostream>
-// #include<string>
-// #include<map>
-// using namespace std;
-// int main(){
-//   map<string,string> m;
-//   m["HELLO"]="ENGLISH";
-//   m["HOLA"]="SPANISH";
-//   m["HALLO"]="GERMAN";
-//   m["BONJOUR"]="FRENCH";
-//   m["CIAO"]="ITALIAN";
-//   m["ZDRAVSTVUJTE"]="RUSSIAN";
-//   string a;
-//   int count1=1;
-//   while(cin>>a && a!="#"){
-//     if(m.count(a)==0)cout<<"Case "<<count1<<": UNKNOWN"<<endl;
-//     else{cout<<"Case "<<count1<<": "<<m[a]<<endl;
+#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+int main(){
+  map<string,string> m;
+  m["HELLO"]="ENGLISH";
+  m["HOLA"]="SPANISH";
+  m["HALLO"]="GERMAN";
+  m["BONJOUR"]="FRENCH";
+  m["CIAO"]="ITALIAN";
+  m["ZDRAVSTVUJTE"]="RUSSIAN";
+  string a;
+  int count1=1;
+  while(cin>>a && a!="#"){
+    if(m.count(a)==0)cout<<"Case "<<count1<<": UNKNOWN"<<endl;
+    else{cout<<"Case "<<count1<<": "<<m[a]<<endl;
     
-//   }count1++;
-//   }
-// }
+  }count1++;
+  }
+}
