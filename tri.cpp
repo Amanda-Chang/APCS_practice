72f4333a1bda72af8d0c0b3943d4ec70b816e895
Amanda-Chang <78400863+Amanda-Chang@users.noreply.github.com>
2021-03-28 15:38:56 +0800

    Update tri.cpp

--- tri.cpp
+++ tri.cpp
@@ -1,15 +1,15 @@
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
-// int main(){
-//   int a[3];
-//   cin>>a[0]>>a[1]>>a[2];
-//   sort(a,a+3);
-//   cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
-//   if((a[0]+a[1])<=a[2])cout<<"No\n";
-//   else if((a[0]*a[0]+a[1]*a[1])<a[2]*a[2])           cout<<"Obtuse\n";
-//   else if((a[0]*a[0]+a[1]*a[1])==a[2]*a[2])          cout<<"Right\n";
-//   else if((a[0]*a[0]+a[1]*a[1])>a[2]*a[2])           cout<<"Acute\n";
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+  int a[3];
+  cin>>a[0]>>a[1]>>a[2];
+  sort(a,a+3);
+  cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
+  if((a[0]+a[1])<=a[2])cout<<"No\n";
+  else if((a[0]*a[0]+a[1]*a[1])<a[2]*a[2])           cout<<"Obtuse\n";
+  else if((a[0]*a[0]+a[1]*a[1])==a[2]*a[2])          cout<<"Right\n";
+  else if((a[0]*a[0]+a[1]*a[1])>a[2]*a[2])           cout<<"Acute\n";
   
   
-// }
+}
