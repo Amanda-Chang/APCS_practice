3b395381158614d8068b1eb6690f2f4625cca26a
Amanda-Chang <78400863+Amanda-Chang@users.noreply.github.com>
2021-03-28 15:37:52 +0800

    Update secret.cpp

--- secret.cpp
+++ secret.cpp
@@ -1,18 +1,18 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//   int a=0;
-//   cin>>a;
-//   int a1,a2=0;
-//   int len=0;
-//   while(a>0){
-//     len++;
-//     a/=10;
-//   }
-//   int b[len];
-//   for(int i=0;i<len;i++){
-//     if((b[i]%2)!=0)a1+=b[i];
-//     if((b[i]%2)==0)a2+=b[i];
-//   }
-//   cout<<abs(a1-a2);
-// }
+#include<iostream>
+using namespace std;
+int main(){
+  int a=0;
+  cin>>a;
+  int a1,a2=0;
+  int len=0;
+  while(a>0){
+    len++;
+    a/=10;
+  }
+  int b[len];
+  for(int i=0;i<len;i++){
+    if((b[i]%2)!=0)a1+=b[i];
+    if((b[i]%2)==0)a2+=b[i];
+  }
+  cout<<abs(a1-a2);
+}
