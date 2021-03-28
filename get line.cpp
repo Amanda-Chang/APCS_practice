c5b040bf645fb6478e074544ebf6d0c1815457ea
Amanda-Chang <78400863+Amanda-Chang@users.noreply.github.com>
2021-03-28 15:34:06 +0800

    Update get line.cpp

--- get line.cpp
+++ get line.cpp
@@ -1,8 +1,8 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
-// int main(){
-//   string str;
-//   getline(cin,str);
-//   cout<<str<<endl;
-// }
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string str;
+  getline(cin,str);
+  cout<<str<<endl;
+}
